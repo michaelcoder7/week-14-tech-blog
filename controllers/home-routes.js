0140b9b3f8081ecb042bfb10db4624dc711c9b06
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-16 23:25:32 +0100

    home routes code in progress

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,3 +27,33 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// GET one blog only, and by id
+router.get("/blog/:id", withAuth, async (req, res) => {
+  // If the user is logged in, allow them to view the blog
+  try {
+    const dbBlogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            },
+          ],
+        },
+      ],
+    });
+    if (dbBlogData) {
+      const blog = dbBlogData.get({ plain: true });
+      res.render("blog", { blog, loggedIn: req.session.loggedIn });
+    } else {
+      res.status(404).json({ message: "No Blog found with that id!" });
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
