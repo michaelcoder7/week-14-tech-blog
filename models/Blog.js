1a3e79685306a1a8a33b9c2cc9b4b9ba1e7d32b7
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-17 03:04:30 +0100

    model blog file code completed

--- models/Blog.js
+++ models/Blog.js
@@ -3,19 +3,38 @@ const sequelize = require("../config/connection");
 
 class Blog extends Model {}
 
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
   },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-});
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    timestamps: true,
+    modelName: "blog",
+  }
+);
+
+module.exports = Blog;
