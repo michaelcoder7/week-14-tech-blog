7a8a2dae3273765d691b5a00a22fd71c52bbe502
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-17 03:02:55 +0100

    model blogs code in progress

---
+++ models/Blog.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Blog extends Model {}
+
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
