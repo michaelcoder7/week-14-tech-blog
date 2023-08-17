a619b0d537f4640332d59cee91b83849f9cd9758
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-17 02:53:57 +0100

    model index file updated

--- models/index.js
+++ models/index.js
@@ -1,13 +1,32 @@
-const User = require('./User');
-const Project = require('./Project');
+const User = require("./User");
+const Blog = require("./Blog");
+const Comment = require("./Comment");
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
 });
 
-module.exports = { User, Project };
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: "blog_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Blog, Comment };
