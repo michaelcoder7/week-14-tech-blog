b3fc8a2d35ef6ed1cf5cfb0535c395eac78717f3
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-17 15:26:08 +0100

    public dashboard js code in progress

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,3 +5,27 @@ const createButton = document.querySelector("#crate");
 newPostButton.addEventListener("click", () => {
   articleElement.classList.remove("d-none");
 });
+
+const submitBlogHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#title").value.trim();
+  const description = document.querySelector("#description").value.trim();
+  if (title && description) {
+    const response = await fetch(`/dashboard`, {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        description,
+        description,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/dashboard`);
+    } else {
+      alert("Failed to add blog.");
+    }
+  }
+};
