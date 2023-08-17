c0f6768ad98f40e0b3ece59ccc30b9b888d395c2
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-17 09:21:34 +0100

    seeds js code in progress

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,9 @@
-const sequelize = require('../config/connection');
-const { User } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Blog, Comment } = require("../models");
 
-const userData = require('./userData.json');
+const userData = require("./userData.json");
+const blogData = require("./blogData.json");
+const commentData = require("./commentData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
