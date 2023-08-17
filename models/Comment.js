dcdccf2fa254bf45dac1c3c2a89a331c5006e19f
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-08-17 02:59:59 +0100

    model comment file code in progress

---
+++ models/Comment.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  blog_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "blog",
+      key: "id",
+    },
+  },
+});
